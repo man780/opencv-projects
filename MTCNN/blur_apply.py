bcaa8473254db5f4cdd2fd21766dbd9021b88200
murattatibaev <M.Tatibayev@uzinfocom.uz>
2024-07-22 18:24:55 +0500

    face detection with MTCNN

--- MTCNN/blur_apply.py
+++ MTCNN/blur_apply.py
@@ -23,3 +23,40 @@ def blur_apply(image, depth):
         image[y:y + h, x:x + w] = face
 
     return cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+
+def detect_face(image):
+    detector = MTCNN()
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    faces = detector.detect_faces(image)
+    for face in faces:
+        x, y, w, h = face['box']
+        cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    return cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+
+def detect_face_2(image):
+    detector = MTCNN()
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    result = detector.detect_faces(image)
+
+    # Result is an array with all the bounding boxes detected. We know that for 'ivan.jpg' there is only one.
+    bounding_box = result[0]['box']
+    keypoints = result[0]['keypoints']
+
+    cv2.rectangle(image,
+                  (bounding_box[0], bounding_box[1]),
+                  (bounding_box[0] + bounding_box[2], bounding_box[1] + bounding_box[3]),
+                  (0, 155, 255),
+                  2)
+
+    cv2.circle(image, (keypoints['left_eye']), 2, (0, 155, 255), 2)
+    cv2.circle(image, (keypoints['right_eye']), 2, (0, 155, 255), 2)
+    cv2.circle(image, (keypoints['nose']), 2, (0, 155, 255), 2)
+    cv2.circle(image, (keypoints['mouth_left']), 2, (0, 155, 255), 2)
+    cv2.circle(image, (keypoints['mouth_right']), 2, (0, 155, 255), 2)
+
+    cv2.imwrite(image, cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+
+    print(result)
+    return cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
--- first.py
+++ first.py
@@ -6,7 +6,7 @@ from io import BytesIO
 
 import numpy as np
 import cv2
-from MTCNN.blur_apply import blur_apply
+from MTCNN.blur_apply import blur_apply, detect_face, detect_face_2
 
 """
 * Optional
@@ -20,10 +20,14 @@ tf.config.set_visible_devices([], 'GPU')
 #ReadMe 
 * repositories : ['mtcnn':'face detection', 'tensorflow':'core face detection']
 """
-app = FastAPI()
+app = FastAPI(
+    title="OpenCV Detection",
+    description="Detection APIs",
+    version="1.0.0"
+)
 
 
-@app.post("/filters/blur")
+@app.post("/face/blur", tags=["Face"])
 async def image_upload(
         file: UploadFile = File(..., description="Uploaded File"),
         depth: int = Form(30, title="Depth", description="Blur Depth", ge=0, le=100)
@@ -36,6 +40,38 @@ async def image_upload(
     i_buffer = np.frombuffer(file_content, np.uint8)
     image = cv2.imdecode(i_buffer, cv2.IMREAD_COLOR)
     processed_image = blur_apply(image, depth)
+
+    _, img_encoded = cv2.imencode('.jpg', processed_image)
+    return StreamingResponse(BytesIO(img_encoded.tobytes()), media_type="image/jpeg")
+
+
+@app.post("/face/detect", tags=["Face"])
+async def face_detect(
+        file: UploadFile = File(..., description="Uploaded File"),
+):
+    content_type = file.content_type
+    if content_type not in ["image/jpeg"]:
+        raise HTTPException(status_code=422, detail={"error": str("Invalid content type")})
+    file_content = await file.read()
+    i_buffer = np.frombuffer(file_content, np.uint8)
+    image = cv2.imdecode(i_buffer, cv2.IMREAD_COLOR)
+    processed_image = detect_face(image)
+
+    _, img_encoded = cv2.imencode('.jpg', processed_image)
+    return StreamingResponse(BytesIO(img_encoded.tobytes()), media_type="image/jpeg")
+
+
+@app.post("/face/detect2", tags=["Face"])
+async def face_detect2(
+        file: UploadFile = File(..., description="Uploaded File"),
+):
+    content_type = file.content_type
+    if content_type not in ["image/jpeg"]:
+        raise HTTPException(status_code=422, detail={"error": str("Invalid content type")})
+    file_content = await file.read()
+    i_buffer = np.frombuffer(file_content, np.uint8)
+    image = cv2.imdecode(i_buffer, cv2.IMREAD_COLOR)
+    processed_image = detect_face_2(image)
 
     _, img_encoded = cv2.imencode('.jpg', processed_image)
     return StreamingResponse(BytesIO(img_encoded.tobytes()), media_type="image/jpeg")
